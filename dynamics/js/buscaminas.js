9dbf6ba4ea0f8bcfd11e0ac8048d44356548a6f5
BrunoEduar <104513620+BrunoEduar@users.noreply.github.com>
2022-05-18 00:04:59 -0500

    cambio

--- dynamics/js/buscaminas.js
+++ dynamics/js/buscaminas.js
@@ -1,1 +1,19 @@
-d
+const tamanio = document.getElementsByClassName("tamaño");
+const padre = document.getElementById("padre");
+const button = document.getElementById("jugar");
+const div = document.getElementById("div");
+
+button.addEventListener("click", (evento) => {
+    div.style.display = "none";
+    switch(padre.value){
+        case '8*8':
+            break;
+        case '16*16':
+            break;
+        case '24*24':
+            break;
+        default:
+            break;
+    }
+});
+
