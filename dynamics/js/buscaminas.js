88f7566273f302906fd9a4570c6cac9e0a30f683
BrunoEduar <104513620+BrunoEduar@users.noreply.github.com>
2022-05-19 16:35:38 -0500

    Add files via upload

--- dynamics/js/buscaminas.js
+++ dynamics/js/buscaminas.js
@@ -1,19 +1,72 @@
-const tamanio = document.getElementsByClassName("tamaño");
-const padre = document.getElementById("padre");
-const button = document.getElementById("jugar");
-const div = document.getElementById("div");
 
-button.addEventListener("click", (evento) => {
+const button = document.getElementById("juego-nuevo");
+const tablero = document.getElementById("tablero");
+const select = document.getElementById("padre");
+
+switch (select.value) {
+    case '8*8':
+        let filas = 8;
+        let columnas = 8;
+    break;
+    case '16*16':
+        let filas = 16;
+        let columnas = 16;
+    break;
+    case '24*24':
+        let filas = 24;
+        let columnas = 24;
+    break;
+}
+
+let minas = filas * columnas * 0.1;
+let tablero = [];
+
+button.addEventListener("click", gen_ta);
+
+button.addEventListener("click", gen_ta);
+button.addEventListener("click", va_ta);
+button.addEventListener("click", po_Mi);
+button.addEventListener("click", con_Mi);
+
+function gen_ta(){
     div.style.display = "none";
-    switch(padre.value){
-        case '8*8':
-            break;
-        case '16*16':
-            break;
-        case '24*24':
-            break;
-        default:
-            break;
+    for(let f = 0; f<filas; f++){
+        tablero.innerHTML += "<tr>";
+        for(let c=0; c<filas; c++){
+            tablero.innerHTML += "<td class='celdas'></td>";
+        }
     }
-});
+}
 
+function nu_jue{
+    gen_ta();
+    
+}
+
+function va_ta(){
+    tablero = [];
+    for (let c = 0; c < columnas; c++) {
+        tablero.push([]);
+        
+    }
+}
+
+function po_Mi(){
+    for(let m = 0; m < minas; m++){
+        do{
+            r1 = Math.floor(Math.random() * columnas);
+            r2 = Math.floor(Math.random() * filas);
+        }while(tablero[r1][r2] != -1);
+        tablero[r1][r2] = -1;
+    }
+}
+
+function con_Mi(){
+    for(let f = 0; f<filas; f++){
+        for(let c=0; c<filas; c++){
+            if (!tablero[c][f]) {
+                tablero[c][f] = 0;
+            }
+        }
+    }
+}
